909592dde2c27587d1d0656d13841ad8e040fa1d
behmoustapha <diarrassouba2106@gmail.com>
2026-02-22 21:50:37 +0100

    Amélioration de l'ui sur mobile

--- test.php
+++ test.php
@@ -1,15 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
 <?php
-require_once('index.html');
-require_once('test.html');
+// the message
+$msg = "First line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("diarrassouba2106@gmail.com","My subject",$msg)
 ?>
